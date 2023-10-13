18e4873af80c5cf9de50b5aac6e11f26d2547f26
Teo <8duchuynh@gmail.com>
2023-10-13 20:20:04 +0700

    Add project files.

---
+++ CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+    }
+}
